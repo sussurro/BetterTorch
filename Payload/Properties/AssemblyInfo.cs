e17d03c8bc86a8d19e59e16866d1e09cb7c63370
sussurro <sussurro@happypacket.net>
2019-09-05 08:57:18 -0400

    Fixed the GUID

--- Payload/Properties/AssemblyInfo.cs
+++ Payload/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 //[assembly: Guid("56598f1c-6d88-4994-a392-af337abe5777")]
 //[assembly: Guid("1138506a-b949-46a7-b6c0-ee26499fdeaf")]
-[assembly: Guid("{46C166AA-3108-11D4-9348-00C04F8EEB71}")]
+[assembly: Guid("46C166AA-3108-11D4-9348-00C04F8EEB71")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
